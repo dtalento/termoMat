44213af30fdfdff0c5871658420bcede0d37ce0a
dtalento <damiantalento@gmail.com>
2023-10-27 18:36:30 -0300

    added test configuration

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pyromat as pyro
+from pyscript import document
 
+def testBtn():
+    testDiv = document.querySelector('#test-output')
+    testDiv.innerText = "Test"
