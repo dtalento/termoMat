6c929998b1969b5466acabff171b2b5a680f5ebe
dtalento <damiantalento@gmail.com>
2023-11-03 15:22:07 -0300

    set Table Format

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ dataContainer.innerText = 'Choose Things'
 fig1, ax1 = plt.subplots()
 # ax1.plot(h, p)
 
-def formatOutput(vec1, vec2):
-    output = "<tr><th>#</th> <th>P(kPa)</th></tr>"
+def formatOutput(varName, fixedName, vec1, fixedValue):
+    output = f"<theader><th colspan=4>{fixedName} = {fixedValue}</th></theader>"
+    output += "<tr><th>-</th> <th>P(kPa)</th> <th>-</th><th>-</th></tr>"
     i = 0
     for value in vec1:
         i += 1
-        newrow = f"<tr><td>{i}</td> <td>{value}</td></tr>"
+        newrow = f"<tr><td>-</td> <td>{value}</td> <td>-</td> <td>-</td> </tr>"
         output += newrow
     return str(output)
 
@@ -35,5 +36,5 @@ def testBtn():
     fixedValue = float(document.querySelector('#fixed-value').value)
 
     p = np.linspace(rangeMin, rangeMax, step)
-    dataContainer.innerHTML = formatOutput(p, p)
+    dataContainer.innerHTML = formatOutput("p", "T", p, fixedValue)
     # display(fig1, target='test-output', append=False)
