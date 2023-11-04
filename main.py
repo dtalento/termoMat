da50ea5f71cfaea09c8e5f857e95c44593fd030f
dtalento <damiantalento@gmail.com>
2023-11-03 23:53:37 -0300

    New Data Structure

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from js import console, window, document # type: ignore
 from pyscript import display # type: ignore
 
 pm.config['unit_pressure'] = 'kPa'
-pm.config['unit_temperature'] = 'K'
+pm.config['unit_temperature'] = 'C'
 sust = pm.get('mp.H2O')
 
 dataContainer = document.querySelector('#data-container')
@@ -33,6 +33,87 @@ def formatOutput(output):
         formattedOutput += newrow
     return str(formattedOutput)
 
+def getVarT(T, varName, min, max, step):
+    match varName:
+        case 'T':
+            raise Exception('Invalid Input')
+        case 'p':
+            p = np.linspace(min, max, step)
+            u = sust.e(T=T, p=p)
+            h = sust.h(T=T, p=p)
+            s = sust.s(T=T, p=p)
+        case 'u':
+            u = np.linspace(min, max, step)
+            p = sust.p(T=T, e=u)
+            h = sust.h(T=T, e=u)
+            s = sust.s(T=T, e=u)
+        case 'h':
+            h = np.linspace(min, max, step)
+            p = sust.p(T=T, h=h)
+            u = sust.e(T=T, h=h)
+            s = sust.s(T=T, h=h)
+        case 's':
+            s = np.linspace(min, max, step)
+            p = sust.p(T=T, s=s)
+            u = sust.e(T=T, s=s)
+            h = sust.h(T=T, s=s)
+        case other:
+            raise Exception('PYROMAT ERROR')   
+    return p, u, h, s
+
+def getVarP(p, varName, min, max, step):
+    match varName:
+        case 'p':
+            raise Exception('Invalid Input')
+        case 'T':
+            T = np.linspace(min, max, step)
+            u = sust.e(T=T, p=p)
+            h = sust.h(T=T, p=p)
+            s = sust.s(T=T, p=p)
+        case 'u':
+            u = np.linspace(min, max, step)
+            T = sust.T(p=p, e=u)
+            h = sust.h(p=p, e=u)
+            s = sust.s(p=p, e=u)
+        case 'h':
+            h = np.linspace(min, max, step)
+            T = sust.p(p=p, h=h)
+            u = sust.e(p=p, h=h)
+            s = sust.s(p=p, h=h)
+        case 's':
+            s = np.linspace(min, max, step)
+            T = sust.p(p=p, s=s)
+            u = sust.e(p=p, s=s)
+            h = sust.h(p=p, s=s)
+        case other:
+            raise Exception('PYROMAT ERROR')   
+    return p, u, h, s
+
+
+def getData(input):
+    varName =   input['varName']
+    min = input['min']
+    max = input['max']
+    step = input['step']
+    varUnit =   input['varUnit']
+    fixedName = input['fixedName']
+    fixedValue =input['fixedValue']
+    fixedUnit = input['fixedUnit']
+
+    match fixedName:
+        case 'T':
+            T = fixedValue*np.ones(step)
+            p, u, h, s = getVarT(fixedValue, varName, min, max, step)
+        case 'p':
+            p = fixedValue*np.ones(step)
+            T, u, h, s = getVarP(fixedValue, varName, min, max, step)
+        case other:
+            raise Exception('INVALID PROPERTY')
+
+    DATA = {'T':T, 'p':p, 'u':u, 'h':h, 's':s}
+
+    return DATA
+
 def getInputs():
     pptyVariable = document.querySelector('#ppty-variable').value
     pptyFixed = document.querySelector('#ppty-fixed').value
@@ -52,11 +133,11 @@ def testBtn():
     except:
         dataContainer.innerHTML = "<p>Invalid Input</p>"
     else:    
-        rangeMin = input['min']
-        rangeMax = input['max']
-        step = input['step']
-
-        output = input.copy()
-        output['varValues'] = np.linspace(rangeMin, rangeMax, step)
-        dataContainer.innerHTML = formatOutput(output)
-        # display(fig1, target='test-output', append=False)
+        try:
+           DATA = getData(input)
+        except Exception as error:
+           dataContainer.innerHTML = error
+        else:
+            # dataContainer.innerHTML = formatOutput(output)
+            # display(fig1, target='test-output', append=False)
+            pass
